3475b1c6cd7c3eb0f33a30fe6b0e790c016b1f20
sunjones0404 <david@sunjones.com>
2019-07-14 11:10:12 +0100

    Added JS for modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-// Define selectors
-const skillsBtn = document.getElementById('skills'),
-	workBtn = document.getElementById('work');
+// Button selectors
+const btn = {
+	skills: document.getElementById('skills'),
+	skillsClose: document.getElementById('skills-modal-close')
+};
+
+//Modal selectors
+const modal = {
+	skills: document.getElementById('skills-modal')
+};
+
+btn.skills.addEventListener('click', e => {
+	modal.skills.style.display = 'flex';
+});
+
+btn.skillsClose.addEventListener('click', e => {
+	modal.skills.style.display = 'none';
+});

1253592a189ed2b9665b4cccec95bde1c56eace9
sunjones0404 <david@sunjones.com>
2019-11-23 20:25:34 +0000

    Dark mode enabled

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 // Button selectors
 const btn = {
-	skills: document.getElementById('skills'),
-	skillsClose: document.getElementById('skills-modal-close')
+  skills: document.getElementById('skills'),
+  skillsClose: document.getElementById('skills-modal-close'),
+  darkModeBtn: document.getElementById('darkMode')
 };
+
+const body = document.querySelector('body');
 
 //Modal selectors
 const modal = {
-	skills: document.getElementById('skills-modal')
+  skills: document.getElementById('skills-modal')
 };
 
 btn.skills.addEventListener('click', e => {
-	modal.skills.style.display = 'flex';
+  modal.skills.style.display = 'flex';
 });
 
-btn.skillsClose.addEventListener('click', e => {
-	modal.skills.style.display = 'none';
+btn.skillsClose.addEventListener('click', () => {
+  modal.skills.style.display = 'none';
+});
+
+body.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.classList.contains('modal-wrapper')) {
+    modal.skills.style.display = 'none';
+  }
+});
+
+btn.darkModeBtn.addEventListener('click', () => {
+  if (body.classList.contains('light')) {
+    body.classList.replace('light', 'dark');
+    btn.darkModeBtn.innerHTML = '<i class="far fa-adjust"></i> Light Mode';
+  } else {
+    body.classList.replace('dark', 'light');
+    btn.darkModeBtn.innerHTML = '<i class="far fa-adjust"></i> Dark Mode';
+  }
 });

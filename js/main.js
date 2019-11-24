db864597ec0370f53d59e558178e4ea73b1878d4
sunjones0404 <david@sunjones.com>
2019-11-24 15:30:54 +0000

    Rotating profile image

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const btn = {
   skills: document.getElementById('skills'),
   skillsClose: document.getElementById('skills-modal-close'),
-  darkModeBtn: document.getElementById('darkMode')
+  darkModeBtn: document.getElementById('darkMode'),
+  img: document.querySelector('.img')
 };
 
 const body = document.querySelector('body');
@@ -35,4 +36,11 @@ btn.darkModeBtn.addEventListener('click', () => {
     body.classList.replace('dark', 'light');
     btn.darkModeBtn.innerHTML = '<i class="far fa-adjust"></i> Dark Mode';
   }
+});
+
+let count = 20;
+
+btn.img.addEventListener('click', () => {
+  count += 20;
+  btn.img.style.transform = `rotate(${count}deg)`;
 });
